2898e494a7829262291f483cec37b2e20e48b209
cyril-gao <cyril.gao@outlook.com>
2020-10-14 11:36:40 -0400

    How to insert elements into or delete elements from a red black tree or an AVL tree

---
+++ BST/test_bst_tree.py
@@ -0,0 +1,140 @@
+import unittest
+import random
+from multiprocessing import Process, Queue, cpu_count
+from rbtree import RedBlackTree
+from avltree import AVLTree
+
+
+class Request:
+    def __init__(self, input, clazz):
+        self.input = tuple(input)
+        self.clazz = clazz
+
+    def __hash__(self):
+        return 37 * hash(self.input) + 29 * hash(self.clazz)
+
+    def __repr__(self):
+        return "Request(%s for %s)" % (self.input, self.clazz)
+
+
+class Response:
+    def __init__(self, id, ok, input=None, rinput=None, v=None, excep=None):
+        self.id = id
+        self.ok = ok
+        self.input = input
+        self.rinput = rinput
+        self.v = v
+        self.excep = excep
+
+    def __repr__(self):
+        if self.ok:
+            return "%s: OK" % self.id
+        else:
+            return "%s\n%s\n%s\n%s" % (self.input, self.rinput, self.v, self.excep)
+
+
+def get_hybrid_operations(input):
+    class Operation:
+        def __init__(self, key, inserting=True):
+            self.key = key
+            self.inserting = inserting
+        def __repr__(self):
+            return "%s %s" % ("insert" if self.inserting else "delete", self.key)
+
+    retval = []
+    deleting = []
+    upper_limit = len(input)
+    start = 0
+    while start < upper_limit:
+        end = min(start + 20, upper_limit)
+        v = random.randint(start, end)
+        if v == start:
+            v += 1
+        for i in range(start, v):
+            retval.append(Operation(input[i]))
+        deleting.extend(input[start:v])
+        start = v
+        dl = len(deleting)
+        if dl > 0:
+            v = random.randint(0, dl)
+            if v == 0:
+                v = 1
+            for i in range(0, v):
+                retval.append(Operation(deleting[i], False))
+            deleting = deleting[v:]
+    for v in deleting:
+        retval.append(Operation(v, False))
+    return retval
+
+
+def run_case(request_queue, response_queue):
+    while True:
+        request = request_queue.get()
+        id = hash(request)
+        input = list(request.input)
+        rinput = []
+        v = None
+        try:
+            operations = get_hybrid_operations(input)
+            tree = RedBlackTree() if request.clazz == "RedBlackTree" else AVLTree()
+            for o in operations:
+                if o.inserting:
+                    tree.put(o.key)
+                    assert o.key in tree
+                else:
+                    assert o.key in tree
+                    del tree[o.key]
+
+            for v in input:
+                tree.put(v)
+                assert v in tree
+                assert tree.valid()
+            rinput = input[:]
+            random.shuffle(rinput)
+            for v in rinput:
+                del tree[v]
+                assert v not in tree
+                assert tree.valid()
+            r = Response(id, True)
+            response_queue.put(r)
+        except AttributeError as ae:
+            r = Response(id, False, input, rinput, v, ae)
+            response_queue.put(r)
+
+
+class BSTTester(unittest.TestCase):
+    def setUp(self):
+        self.request_queue = Queue()
+        self.response_queue = Queue()
+        cpu = cpu_count()
+        self.workers = [Process(target=run_case, args=(self.request_queue, self.response_queue)) for _ in range(cpu)]
+
+    def test_trees(self):
+        CASE_NUMBER = 32
+        LOWER_LIMIT = 512
+        UPPER_LIMIT = 1024*4
+        for _ in range(CASE_NUMBER):
+            input = [_ for _ in range(random.randint(LOWER_LIMIT, UPPER_LIMIT))]
+            r = Request(input, "RedBlackTree")
+            self.request_queue.put(r)
+            r = Request(input, "AVLTree")
+            self.request_queue.put(r)
+            random.shuffle(input)
+
+        for w in self.workers:
+            w.start()
+
+        wrong = False
+        for _ in range(CASE_NUMBER * 2):
+            r = self.response_queue.get()
+            if not r.ok:
+                print(r)
+                wrong = True
+
+        for w in self.workers:
+            w.terminate()
+
+        self.request_queue.close()
+        self.response_queue.close()
+
+        assert not wrong
