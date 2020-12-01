2e52d9b0cafcd382373640a4860b5c53132155cb
cyril-gao <cyril.gao@outlook.com>
2020-11-30 23:42:15 -0500

    leetcode: resolve some problems related to tree

--- Algorithms/leetcode/Java/AlgorithmsStudy/src/main/java/com/cybernumen/BinaryTree.java
+++ Algorithms/leetcode/Java/AlgorithmsStudy/src/main/java/com/cybernumen/BinaryTree.java
@@ -117,4 +117,158 @@ public class BinaryTree {
         }
         return retval;
     }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root != null) {
+            if (root.left == null && root.right == null && root.val == sum) {
+                return true;
+            }
+
+            int remains = sum - root.val;
+            if (hasPathSum(root.left, remains) || hasPathSum(root.right, remains)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left != null && right != null) {
+            if (left.val == right.val) {
+                if (isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root != null) {
+            return isSymmetric(root.left, root.right);
+        }
+        return true;
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> retval = new LinkedList<>();
+        if (root != null) {
+            List<TreeNode> current = new LinkedList<>();
+            current.add(root);
+            boolean left = true;
+            while (!current.isEmpty()) {
+                List<Integer> row = new LinkedList<>();
+                for (TreeNode i : current) {
+                    row.add(i.val);
+                }
+
+                left = !left;
+                List<TreeNode> next = new LinkedList<>();
+                for (int i = current.size() - 1; i >= 0; --i) {
+                    TreeNode t = current.get(i);
+                    if (left) {
+                        if (t.left != null) {
+                            next.add(t.left);
+                        }
+                    } else {
+                        if (t.right != null) {
+                            next.add(t.right);
+                        }
+                    }
+                    if (left) {
+                        if (t.right != null) {
+                            next.add(t.right);
+                        }
+                    } else {
+                        if (t.left != null) {
+                            next.add(t.left);
+                        }
+                    }
+                }
+                retval.add(row);
+                current = next;
+            }
+        }
+        return retval;
+    }
+
+    public void inorderTraversalRecursively(TreeNode root, List<Integer> output) {
+        if (root != null) {
+            inorderTraversalRecursively(root.left, output);
+            output.add(root.val);
+            inorderTraversalRecursively(root.right, output);
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> retval = new LinkedList<>();
+        if (root != null) {
+            Stack<TreeNode> stack = new Stack<>();
+            for (TreeNode p = root; p != null || !stack.isEmpty();) {
+                while (p != null) {
+                    stack.push(p);
+                    p = p.left;
+                }
+                p = stack.pop();
+                retval.add(p.val);
+                p = p.right;
+            }
+        }
+        return retval;
+    }
+
+    private static class BstInternal {
+        boolean valid;
+        boolean exist;
+        int minValue;
+        int maxValue;
+
+        public BstInternal(boolean v, boolean e, int i, int a) {
+            valid = v;
+            exist = e;
+            minValue = i;
+            maxValue = a;
+        }
+
+        public BstInternal() {
+            this(true, false, 0, 0);
+        }
+    }
+
+    private static BstInternal getBstInternal(TreeNode root) {
+        BstInternal retval = new BstInternal();
+        if (root != null) {
+            retval.valid = false;
+            if ((root.left == null || root.left.val < root.val) && (root.right == null || root.right.val > root.val)) {
+                BstInternal lr = getBstInternal(root.left);
+                BstInternal rr = getBstInternal(root.right);
+                if (lr.valid && rr.valid) {
+                    if ((!lr.exist || (lr.minValue <= lr.maxValue && lr.maxValue < root.val))
+                            && (!rr.exist || (rr.minValue <= rr.maxValue && rr.minValue > root.val))) {
+                        retval.valid = true;
+                        retval.exist = true;
+                        if (rr.exist) {
+                            retval.maxValue = rr.maxValue;
+                        } else {
+                            retval.maxValue = root.val;
+                        }
+                        if (lr.exist) {
+                            retval.minValue = lr.minValue;
+                        } else {
+                            retval.minValue = root.val;
+                        }
+                    }
+                }
+            }
+        }
+        return retval;
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        BstInternal bi = getBstInternal(root);
+        return bi.valid;
+    }
 }
--- Algorithms/leetcode/Java/AlgorithmsStudy/src/test/java/com/cybernumen/BinaryTreeTest.java
+++ Algorithms/leetcode/Java/AlgorithmsStudy/src/test/java/com/cybernumen/BinaryTreeTest.java
@@ -3,53 +3,130 @@ package com.cybernumen;
 import java.util.*;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
 import org.testng.annotations.*;
 
 public class BinaryTreeTest {
     @Test
-    public void testPreorderTraversal() {
+    public void testTraversal() {
         BinaryTree bt = new BinaryTree();
-        {
-            var n1 = new BinaryTree.TreeNode(1);
-            var n2 = new BinaryTree.TreeNode(2);
-            var n3 = new BinaryTree.TreeNode(3);
-            var n4 = new BinaryTree.TreeNode(4);
-            var n5 = new BinaryTree.TreeNode(5);
-            var n6 = new BinaryTree.TreeNode(6);
-            var n7 = new BinaryTree.TreeNode(7);
-            var n8 = new BinaryTree.TreeNode(8);
-            var n9 = new BinaryTree.TreeNode(9);
-            var n10 = new BinaryTree.TreeNode(10);
-            var n11 = new BinaryTree.TreeNode(11);
-            var n12 = new BinaryTree.TreeNode(12);
-            var n13 = new BinaryTree.TreeNode(13);
-            var n14 = new BinaryTree.TreeNode(14);
-            var n15 = new BinaryTree.TreeNode(15);
-            var n16 = new BinaryTree.TreeNode(16);
-            var n17 = new BinaryTree.TreeNode(17);
-            var n18 = new BinaryTree.TreeNode(18);
-            n1.left = n2;
-            n1.right = n3;
-            n2.left = n4;
-            n2.right = n5;
-            n3.right = n6;
-            n4.right = n7;
-            n5.right = n8;
-            n6.left = n9;
-            n6.right = n10;
-            n7.left = n11;
-            n7.right = n12;
-            n8.right = n13;
-            n9.left = n14;
-            n10.right = n15;
-            n11.left = n16;
-            n13.left = n17;
-            n15.left = n18;
 
+        var n1 = new BinaryTree.TreeNode(1);
+        var n2 = new BinaryTree.TreeNode(2);
+        var n3 = new BinaryTree.TreeNode(3);
+        var n4 = new BinaryTree.TreeNode(4);
+        var n5 = new BinaryTree.TreeNode(5);
+        var n6 = new BinaryTree.TreeNode(6);
+        var n7 = new BinaryTree.TreeNode(7);
+        var n8 = new BinaryTree.TreeNode(8);
+        var n9 = new BinaryTree.TreeNode(9);
+        var n10 = new BinaryTree.TreeNode(10);
+        var n11 = new BinaryTree.TreeNode(11);
+        var n12 = new BinaryTree.TreeNode(12);
+        var n13 = new BinaryTree.TreeNode(13);
+        var n14 = new BinaryTree.TreeNode(14);
+        var n15 = new BinaryTree.TreeNode(15);
+        var n16 = new BinaryTree.TreeNode(16);
+        var n17 = new BinaryTree.TreeNode(17);
+        var n18 = new BinaryTree.TreeNode(18);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.right = n6;
+        n4.right = n7;
+        n5.right = n8;
+        n6.left = n9;
+        n6.right = n10;
+        n7.left = n11;
+        n7.right = n12;
+        n8.right = n13;
+        n9.left = n14;
+        n10.right = n15;
+        n11.left = n16;
+        n13.left = n17;
+        n15.left = n18;
+        {
             List<Integer> r1 = new LinkedList<>();
             bt.preorderTraversalRecursively(n1, r1);
             var r2 = bt.preorderTraversal(n1);
             assertEquals(r1, r2);
+        }
+        {
+            List<Integer> r1 = new LinkedList<>();
+            bt.inorderTraversalRecursively(n1, r1);
+            var r2 = bt.inorderTraversal(n1);
+            assertEquals(r1, r2);
+        }
+    }
+
+    @Test
+    public void testHasPathSum() {
+        BinaryTree bt = new BinaryTree();
+        {
+            var n5 = new BinaryTree.TreeNode(5);
+            var n4 = new BinaryTree.TreeNode(4);
+            var n8 = new BinaryTree.TreeNode(8);
+            var n11 = new BinaryTree.TreeNode(11);
+            var n13 = new BinaryTree.TreeNode(13);
+            var n4_2 = new BinaryTree.TreeNode(4);
+            var n7 = new BinaryTree.TreeNode(7);
+            var n2 = new BinaryTree.TreeNode(2);
+            var n1 = new BinaryTree.TreeNode(1);
+            n5.left = n4;
+            n5.right = n8;
+            n4.left = n11;
+            n8.left = n13;
+            n8.right = n4_2;
+            n11.left = n7;
+            n11.right = n2;
+            n4.right = n1;
+            assertTrue(bt.hasPathSum(n5, 22));
+        }
+    }
+
+    @Test
+    public void testIsValidBST() {
+        BinaryTree bt = new BinaryTree();
+        {
+            var n3 = new BinaryTree.TreeNode(3);
+            var n2 = new BinaryTree.TreeNode(2);
+            var n1 = new BinaryTree.TreeNode(1);
+            n2.left = n1;
+            assertTrue(bt.isValidBST(n2));
+            n2.right = n3;
+            assertTrue(bt.isValidBST(n2));
+            n2.right = null;
+            n1.right = n3;
+            assertFalse(bt.isValidBST(n2));
+        }
+        {
+            var n1 = new BinaryTree.TreeNode(1);
+            var n3 = new BinaryTree.TreeNode(3);
+            var n4 = new BinaryTree.TreeNode(4);
+            var n5 = new BinaryTree.TreeNode(5);
+            var n6 = new BinaryTree.TreeNode(6);
+            n5.left = n1;
+            n5.right = n4;
+            n4.left = n3;
+            n4.right = n6;
+            assertFalse(bt.isValidBST(n5));
+            assertTrue(bt.isValidBST(n4));
+        }
+        {
+            var n5 = new BinaryTree.TreeNode(5);
+            var n6 = new BinaryTree.TreeNode(6);
+            var n10 = new BinaryTree.TreeNode(10);
+            var n15 = new BinaryTree.TreeNode(15);
+            var n20 = new BinaryTree.TreeNode(20);
+            n10.left = n5;
+            n10.right = n15;
+            n15.left = n6;
+            n15.right = n20;
+
+            assertFalse(bt.isValidBST(n10));
+            assertTrue(bt.isValidBST(n15));
         }
     }
 }
