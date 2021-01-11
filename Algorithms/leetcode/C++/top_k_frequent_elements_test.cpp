b03a382a465a84cc5c51c6f47a0e4476add0fe98
cyril-gao <cyril.gao@outlook.com>
2021-01-11 10:58:23 -0500

    leetcode: 347. Top K Frequent Elements

---
+++ Algorithms/leetcode/C++/top_k_frequent_elements_test.cpp
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include <algorithm>
+#include <vector>
+#include <counting_sort.h>
+#include "check.h"
+
+
+/*
+Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+*/
+
+struct Pair
+{
+    int value;
+    int count;
+
+    Pair(int v, int c) : value(v), count(c) {}
+    Pair(): Pair(0, 0) {}
+};
+
+std::vector<int> top_k_frequent(std::vector<int>& nums, int k_frequent)
+{
+    auto comp = [](Pair p1, Pair p2) {
+        return p1.count > p2.count;
+    };
+    integer_sort(nums.begin(), nums.end());
+    std::vector<Pair> heap;
+    heap.reserve(k_frequent);
+    for (int i = 0, hi = 0, ie = static_cast<int>(nums.size()); i < ie;) {
+        int j = i + 1;
+        int c = 1;
+        while (j < ie && nums[j] == nums[i]) {
+            ++c;
+            ++j;
+        }
+        if (hi < k_frequent) {
+            heap.emplace_back(Pair(nums[i], c));
+            ++hi;
+            if (hi == k_frequent) {
+                std::make_heap(heap.begin(), heap.end(), comp);
+            }
+        } else {
+            if (c > heap[0].count) {
+                std::pop_heap(heap.begin(), heap.end(), comp);
+                heap.back() = Pair(nums[i], c);
+                std::push_heap(heap.begin(), heap.end(), comp);
+            }
+        }
+        i = j;
+    }
+    std::vector<int> retval;
+    retval.reserve(k_frequent);
+    for(auto p : heap) {
+        retval.push_back(p.value);
+    }
+    return retval;
+}
+
+int main()
+{
+    {
+        std::vector<int> nums = {1, 1, 1, 2, 2, 3};
+        auto result = top_k_frequent(nums, 2);
+        std::sort(result.begin(), result.end());
+        examine(
+            result.size() == 2 && result[0] == 1 && result[1] == 2,
+            "top_k_frequent is failed at the line: %d\n", __LINE__
+        );
+        result = top_k_frequent(nums, 3);
+        std::sort(result.begin(), result.end());
+        examine(
+            result.size() == 3 && result[0] == 1 && result[1] == 2 && result[2] == 3,
+            "top_k_frequent is failed at the line: %d\n", __LINE__
+        );
+        result = top_k_frequent(nums, 4);
+        std::sort(result.begin(), result.end());
+        examine(
+            result.size() == 3 && result[0] == 1 && result[1] == 2 && result[2] == 3,
+            "top_k_frequent is failed at the line: %d\n", __LINE__
+        );
+    }
+    {
+        std::vector<int> nums = {1};
+        auto result = top_k_frequent(nums, 1);
+        examine(
+            result.size() == 1 && result[0] == 1,
+            "top_k_frequent is failed at the line: %d\n", __LINE__
+        );
+        result = top_k_frequent(nums, 3);
+        examine(
+            result.size() == 1 && result[0] == 1,
+            "top_k_frequent is failed at the line: %d\n", __LINE__
+        );
+    }
+    {
+        std::vector<int> nums = { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 5, 6, 7, 8, 9, 9 };
+        auto result = top_k_frequent(nums, 2);
+        std::sort(result.begin(), result.end());
+        examine(
+            result.size() == 2 && result[0] == 1 && result[1] == 2,
+            "top_k_frequent is failed at the line: %d\n", __LINE__
+        );
+        result = top_k_frequent(nums, 3);
+        std::sort(result.begin(), result.end());
+        examine(
+            result.size() == 3 && result[0] == 1 && result[1] == 2 && result[2] == 3,
+            "top_k_frequent is failed at the line: %d\n", __LINE__
+        );
+        result = top_k_frequent(nums, 4);
+        std::sort(result.begin(), result.end());
+        examine(
+            result.size() == 4 && result[0] == 1 && result[1] == 2 && result[2] == 3 && result[3] == 9,
+            "top_k_frequent is failed at the line: %d\n", __LINE__
+        );
+    }
+    printf("OK\n");
+    return 0;
+}
