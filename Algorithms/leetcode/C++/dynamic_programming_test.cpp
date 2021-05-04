ada82d5acb69842d792574df8e5e38fd2715fcc6
cyril-gao <yuanming.gao@gmail.com>
2021-05-04 19:31:00 -0400

    5. Longest Palindromic Substring

--- Algorithms/leetcode/C++/dynamic_programming_test.cpp
+++ Algorithms/leetcode/C++/dynamic_programming_test.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <iterator>
 #include <set>
+#include <string>
 #include <vector>
 #include <unordered_set>
 #include <unordered_map>
@@ -64,9 +65,85 @@ void generate_parenthesis_test()
 }
 
 
+/*
+Given a string s, return the longest palindromic substring in s.
+*/
+
+std::string get_longest_palindromic_substring(std::string const& input)
+{
+    using position_t = std::pair<size_t, size_t>;
+    size_t n = input.length();
+    std::string retval;
+    if (n > 0) {
+        retval.push_back(input[0]);
+        size_t ml = 1;
+        std::vector<position_t> cache;
+        for (size_t i = 0; i < n; ++i) {
+            cache.emplace_back(i, i);
+        }
+        for (size_t i = 0, j = 1; j < n; ++i, ++j) {
+            if (input[i] == input[j]) {
+                if (ml < 2) {
+                    ml = 2;
+                    retval.assign(&input[i], &input[i + 2]);
+                }
+                cache.emplace_back(i, j);
+            }
+        }
+        for (size_t n_1 = n - 1; !cache.empty();) {
+            std::vector<position_t> next;
+            for (auto position : cache) {
+                if (
+                    position.first > 0 && position.second < n_1 &&
+                    input[--position.first] == input[++position.second]
+                ) {
+                    size_t l = position.second - position.first + 1;
+                    if (ml < l) {
+                        ml = l;
+                        retval.assign(&input[position.first], &input[position.second] + 1);
+                    }
+                    next.push_back(position);
+                }
+            }
+            cache.swap(next);
+        }
+    }
+    return retval;
+}
+
+void longest_palindromic_substring_test()
+{
+    {
+        auto result = get_longest_palindromic_substring("babad");
+        examine(result.length() == 3, "get_longest_palindromic_substring is failed at the line: %d\n", __LINE__);
+    }
+    {
+        auto result = get_longest_palindromic_substring("cbbd");
+        examine(result.length() == 2, "get_longest_palindromic_substring is failed at the line: %d\n", __LINE__);
+    }
+    {
+        auto result = get_longest_palindromic_substring("abcdef");
+        examine(result.length() == 1, "get_longest_palindromic_substring is failed at the line: %d\n", __LINE__);
+    }
+    {
+        auto result = get_longest_palindromic_substring("a");
+        examine(result.length() == 1, "get_longest_palindromic_substring is failed at the line: %d\n", __LINE__);
+    }
+    {
+        auto result = get_longest_palindromic_substring("abcdcb");
+        examine(result.length() == 5, "get_longest_palindromic_substring is failed at the line: %d\n", __LINE__);
+    }
+    {
+        auto result = get_longest_palindromic_substring("abcdcba");
+        examine(result.length() == 7, "get_longest_palindromic_substring is failed at the line: %d\n", __LINE__);
+    }
+}
+
+
 int main()
 {
     generate_parenthesis_test();
+    longest_palindromic_substring_test();
     printf("OK\n");
     return 0;
 }
