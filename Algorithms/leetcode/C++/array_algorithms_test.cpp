1209176993c392c75e1dd4e4b7625b957d7f92e9
cyril-gao <yuanming.gao@gmail.com>
2021-05-04 17:08:52 -0400

    75. Sort Colors

--- Algorithms/leetcode/C++/array_algorithms_test.cpp
+++ Algorithms/leetcode/C++/array_algorithms_test.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <algorithm>
 #include <iterator>
 #include <vector>
 
@@ -69,9 +70,100 @@ void first_missing_positive_test()
     }
 }
 
+/*
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+*/
+void sort_colors(std::vector<int>& colors)
+{
+    int n = static_cast<int>(colors.size());
+    int zero = -1;
+    auto begin = colors.begin();
+    auto two = begin + n;
+    for (auto i = begin; i < two;) {
+        int c = *i;
+        if (c <= 1) {
+            if (c < 1) {
+                ++zero;
+                std::iter_swap(begin + zero, i);
+            }
+            ++i;
+        } else {
+            --two;
+            std::iter_swap(two, i);
+        }
+    }
+}
+
+void sort_colors_test()
+{
+    {
+        std::vector<int> colors = {2, 1, 0};
+        sort_colors(colors);
+        examine(colors[0] == 0 && colors[1] == 1 && colors[2] == 2, "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+    {
+        std::vector<int> colors = {1, 0};
+        sort_colors(colors);
+        examine(colors[0] == 0 && colors[1] == 1, "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+    {
+        std::vector<int> colors = {0};
+        sort_colors(colors);
+        examine(colors[0] == 0, "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+    {
+        std::vector<int> colors = {1};
+        sort_colors(colors);
+        examine(colors[0] == 1, "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+    {
+        std::vector<int> colors = {2};
+        sort_colors(colors);
+        examine(colors[0] == 2, "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+    {
+        std::vector<int> colors = {};
+        sort_colors(colors);
+        examine(colors.empty(), "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+    {
+        std::vector<int> colors = {2, 0};
+        sort_colors(colors);
+        examine(colors[0] == 0 && colors[1] == 2, "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+    {
+        std::vector<int> colors = {2, 0, 2, 1, 1, 1, 0, 2, 1, 0, 2, 1, 0, 0, 0, 1, 1, 2, 2, 2, 1, 0, 0, 1, 2};
+        sort_colors(colors);
+        examine(std::is_sorted(colors.begin(), colors.end()), "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+    {
+        std::vector<int> colors = {0, 2, 0, 2, 1, 1, 1, 0, 2, 1, 0, 2, 1, 0, 0, 0, 1, 1, 2, 2, 2, 1, 0, 0, 1, 2};
+        sort_colors(colors);
+        examine(std::is_sorted(colors.begin(), colors.end()), "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+    {
+        std::vector<int> colors = {1, 2, 0, 2, 1, 1, 1, 0, 2, 1, 0, 2, 1, 0, 0, 0, 1, 1, 2, 2, 2, 1, 0, 0, 1, 2};
+        sort_colors(colors);
+        examine(std::is_sorted(colors.begin(), colors.end()), "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+    {
+        std::vector<int> colors = {2, 0, 2, 1, 1, 1, 0, 2, 1, 0, 2, 1, 0, 0, 0, 1, 1, 2, 2, 2, 1, 0, 0, 1, 2, 0};
+        sort_colors(colors);
+        examine(std::is_sorted(colors.begin(), colors.end()), "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+    {
+        std::vector<int> colors = {2, 0, 2, 1, 1, 1, 0, 2, 1, 0, 2, 1, 0, 0, 0, 1, 1, 2, 2, 2, 1, 0, 0, 1, 2, 1};
+        sort_colors(colors);
+        examine(std::is_sorted(colors.begin(), colors.end()), "sort_colors is failed at the line: %d\n", __LINE__);
+    }
+}
+
 int main()
 {
     first_missing_positive_test();
+    sort_colors_test();
     printf("OK\n");
     return 0;
 }
