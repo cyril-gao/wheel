db16b9a3bffe94e2cdae150bbb5e293dd06fcf12
cyril-gao <yuanming.gao@gmail.com>
2021-05-05 14:47:40 -0400

    3. Longest Substring Without Repeating Characters

--- Algorithms/leetcode/TypeScript/src/longest.substring.without.repeating.characters.ts
+++ Algorithms/leetcode/TypeScript/src/longest.substring.without.repeating.characters.ts
@@ -4,5 +4,24 @@ without repeating characters.
 */
 
 export function lengthOfLongestSubstring(s: string): number {
-    return 0;
+    let retval = s.length;
+    if (retval > 1) {
+        retval = 1;
+        let begin = 0;
+        let map = new Map<string, number>();
+        map.set(s[0], 0);
+        let currentLength = 1;
+        for (let i = 1; i < s.length; ++i) {
+            let j = map.get(s[i]);
+            map.set(s[i], i);
+            if (j === undefined || j < begin) {
+                ++currentLength;
+                retval = Math.max(retval, currentLength);
+            } else {
+                begin = j + 1;
+                currentLength = i - begin + 1;
+            }
+        }
+    }
+    return retval;
 };
