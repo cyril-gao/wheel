e7204cd53d4e8ab3be9bf6f52a9d391fc20c197a
cyril-gao <yuanming.gao@gmail.com>
2021-05-12 20:39:07 -0400

    29. Divide Two Integers

--- Algorithms/leetcode/C++/number_test.cpp
+++ Algorithms/leetcode/C++/number_test.cpp
@@ -82,9 +82,106 @@ void str_to_int_test()
 }
 
 
+/*
+Given two integers dividend and divisor, divide two integers without using multiplication,
+division, and mod operator.
+
+Return the quotient after dividing dividend by divisor.
+
+The integer division should truncate toward zero, which means losing its fractional part.
+For example, truncate(8.345) = 8 and truncate(-2.7335) = -2.
+
+Note: Assume we are dealing with an environment that could only store integers within the 32-bit
+signed integer range: [-231, 231 - 1]. For this problem, assume that your function returns
+231 - 1 when the division result overflows.
+*/
+
+int positive_divide(unsigned int dividend, unsigned int divisor)
+{
+    int retval = 0;
+    assert(divisor > 0);
+    if (dividend >= divisor) {
+        if (((divisor - 1) & divisor) != 0) {
+            unsigned int p, r;
+            for (p = 1, r = divisor; r <= dividend; p <<= 1, r <<= 1) {}
+            for (p >>= 1, r >>= 1; dividend >= divisor; p >>= 1, r >>= 1) {
+                if (dividend >= r) {
+                    dividend -= r;
+                    retval += static_cast<int>(p);
+                }
+            }
+        } else {
+            int i = 0;
+            for (unsigned int d = divisor; d != 1u; ++i, d >>= 1) {}
+            if (!(divisor == 1 && dividend >= INT_MAX)) {
+                retval = static_cast<int>(dividend >> i);
+            } else {
+                retval = INT_MAX;
+            }
+        }
+    }
+    return retval;
+}
+
+int divide(int dividend, int divisor)
+{
+    assert(divisor != 0);
+    if (dividend >= 0 && divisor > 0) {
+        return positive_divide(dividend, divisor);
+    } else if (dividend <= 0 && divisor < 0) {
+        if (dividend != divisor) {
+            if (dividend != INT_MIN && divisor != INT_MIN) {
+                return positive_divide(-dividend, -divisor);
+            }
+            if (dividend == INT_MIN) {
+                return positive_divide(static_cast<unsigned int>(INT_MAX) + 1u, -divisor);
+            }
+            return 0;
+        } else {
+            return 1;
+        }
+    } else {
+        if (divisor < 0) {
+            if (divisor != INT_MIN) {
+                return -positive_divide(dividend, -divisor);
+            }
+            return 0;
+        }
+        if (divisor != 1) {
+            if (dividend != INT_MIN) {
+                return -positive_divide(-dividend, divisor);
+            }
+            return -positive_divide(static_cast<unsigned int>(INT_MAX) + 1u, divisor);
+        } else {
+            return dividend;
+        }
+    }
+}
+
+void divide_test()
+{
+    int nums[] = {
+        INT_MIN, INT_MIN + 1, INT_MIN + 2, -2834752, -345734, -131072, -9253, -745, -87, -64, -5, -4, -3, -2, -1,
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 64, 128, 534, 9857, 131072, 1048576, 9467786, INT_MAX
+    };
+    for (int a : nums) {
+        for (int b : nums) {
+            if (a != INT_MIN || b != -1) {
+                assert(divide(a, b) == (a / b));
+                examine(divide(a, b) == a / b, "divide is failed for the input: %d / %d\n", a, b);
+            } else {
+                assert(divide(a, b) == INT_MAX);
+                examine(divide(a, b) == INT_MAX, "divide is failed for the input: %d / %d\n", a, b);
+            }
+        }
+    }
+}
+
+
 int main()
 {
     str_to_int_test();
+    divide_test();
     printf("OK\n");
     return 0;
 }
