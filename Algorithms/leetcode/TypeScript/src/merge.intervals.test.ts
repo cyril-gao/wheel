dc36709a0beee9d5744069e7e43fd2d25e09f487
cyril-gao <yuanming.gao@gmail.com>
2021-05-16 16:23:14 -0400

    57. Insert Interval

--- Algorithms/leetcode/TypeScript/src/merge.intervals.test.ts
+++ Algorithms/leetcode/TypeScript/src/merge.intervals.test.ts
@@ -1,4 +1,4 @@
-import { merge } from "./merge.intervals";
+import { merge, insert } from "./merge.intervals";
 
 function equals(m1: number[][], m2: number[][]): boolean {
     let retval = m1.length === m2.length && m1[0].length === m2[0].length;
@@ -32,5 +32,124 @@ test(
         result = merge(intervals);
         expectation = [[0, 5]];
         expect(equals(result, expectation)).toBe(true);
+    }
+);
+
+test(
+    "check the result value of the call to the function insert",
+    () => {
+        let intervals = [[1, 3], [6, 9]];
+        let newInterval = [2, 5];
+        let result = insert(intervals, newInterval);
+        let expectation = [[1, 5], [6, 9]];
+        expect(equals(result, expectation)).toBe(true);
+
+        intervals = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]];
+        newInterval = [4, 8];
+        result = insert(intervals, newInterval);
+        expectation = [[1, 2], [3, 10], [12, 16]];
+        expect(equals(result, expectation)).toBe(true);
+
+        intervals = [[10, 15], [18, 19]];
+        newInterval = [3, 4];
+        result = insert(intervals, newInterval);
+        expectation = [[3, 4], [10, 15], [18, 19]];
+        expect(equals(result, expectation)).toBe(true);
+
+        intervals = [[0, 5], [9, 12]];
+        newInterval = [7, 19];
+        result = insert(intervals, newInterval);
+        expectation = [[0, 5], [7, 19]];
+        expect(equals(result, expectation)).toBe(true);
+
+        intervals = [[0, 5], [8, 9]];
+        newInterval = [3, 4];
+        result = insert(intervals, newInterval);
+        expectation = [[0, 5], [8, 9]];
+        expect(equals(result, expectation)).toBe(true);
+
+        intervals = [];
+        newInterval = [4, 8];
+        result = insert(intervals, newInterval);
+        expectation = [[4, 8]];
+        expect(equals(result, expectation)).toBe(true);
+
+        intervals = [[1, 5]];
+        newInterval = [4, 8];
+        result = insert(intervals, newInterval);
+        expectation = [[1, 8]];
+        expect(equals(result, expectation)).toBe(true);
+
+        intervals = [[1, 5]];
+        newInterval = [2, 3];
+        result = insert(intervals, newInterval);
+        expectation = [[1, 5]];
+        expect(equals(result, expectation)).toBe(true);
+
+        intervals = [[6, 7], [8, 10], [12, 16]];
+        newInterval = [1, 5];
+        result = insert(intervals, newInterval);
+        expectation = [[1, 5], [6, 7], [8, 10], [12, 16]];
+        expect(equals(result, expectation)).toBe(true)
+
+        intervals = [[6, 7], [8, 10], [12, 16]];
+        newInterval = [1, 8];
+        result = insert(intervals, newInterval);
+        expectation = [[1, 10], [12, 16]];
+        expect(equals(result, expectation)).toBe(true)
+
+        intervals = [[6, 7], [8, 10], [12, 16]];
+        newInterval = [1, 13];
+        result = insert(intervals, newInterval);
+        expectation = [[1, 16]];
+        expect(equals(result, expectation)).toBe(true)
+
+        intervals = [[6, 7], [8, 10], [12, 16]];
+        newInterval = [1, 19];
+        result = insert(intervals, newInterval);
+        expectation = [[1, 19]];
+        expect(equals(result, expectation)).toBe(true)
+
+        intervals = [[2, 4], [6, 7], [8, 10], [12, 16]];
+        newInterval = [2, 6];
+        result = insert(intervals, newInterval);
+        expectation = [[2, 7], [8, 10], [12, 16]];
+        expect(equals(result, expectation)).toBe(true)
+
+        intervals = [[2, 4], [6, 7], [8, 10], [12, 16]];
+        newInterval = [2, 9];
+        result = insert(intervals, newInterval);
+        expectation = [[2, 10], [12, 16]];
+        expect(equals(result, expectation)).toBe(true)
+
+        intervals = [[2, 4], [6, 7], [8, 10], [12, 16]];
+        newInterval = [2, 13];
+        result = insert(intervals, newInterval);
+        expectation = [[2, 16]];
+        expect(equals(result, expectation)).toBe(true)
+
+        intervals = [[2, 4], [6, 7], [8, 10], [12, 16]];
+        newInterval = [2, 19];
+        result = insert(intervals, newInterval);
+        expectation = [[2, 19]];
+        expect(equals(result, expectation)).toBe(true)
+
+        intervals = [[2, 4], [6, 7], [8, 10], [12, 16]];
+        newInterval = [13, 15];
+        result = insert(intervals, newInterval);
+        expectation = [[2, 4], [6, 7], [8, 10], [12, 16]];
+        expect(equals(result, expectation)).toBe(true)
+
+        intervals = [[2, 4], [6, 7], [8, 10], [12, 16]];
+        newInterval = [16, 18];
+        result = insert(intervals, newInterval);
+        expectation = [[2, 4], [6, 7], [8, 10], [12, 18]];
+        expect(equals(result, expectation)).toBe(true)
+
+        intervals = [[2, 4], [6, 7], [8, 10], [12, 16]];
+        newInterval = [17, 18];
+        result = insert(intervals, newInterval);
+        expectation = [[2, 4], [6, 7], [8, 10], [12, 16], [17, 18]];
+        expect(equals(result, expectation)).toBe(true)
     }
 );
--- Algorithms/leetcode/TypeScript/src/merge.intervals.ts
+++ Algorithms/leetcode/TypeScript/src/merge.intervals.ts
@@ -70,3 +70,102 @@ export function merge(intervals: number[][]): number[][] {
     retval.push(candidate);
     return retval;
 }
+
+
+/*
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+Example 2:
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+Example 3:
+Input: intervals = [], newInterval = [5,7]
+Output: [[5,7]]
+
+Example 4:
+Input: intervals = [[1,5]], newInterval = [2,3]
+Output: [[1,5]]
+
+Example 5:
+Input: intervals = [[1,5]], newInterval = [2,7]
+Output: [[1,7]]
+*/
+
+function copy(source: number[][], begin: number, end: number, target: number[][]): void {
+    for (; begin < end; ++begin) {
+        target.push([...source[begin]]);
+    }
+}
+
+export function insert(intervals: number[][], newInterval: number[]): number[][] {
+    let retval = [];
+    let end = intervals.length;
+    let firstPosition = find(intervals, 0, end, (range: number[]) => range[0] - newInterval[0]);
+    if (firstPosition !== end) {
+        let secondPosition = find(intervals, firstPosition, end, (range: number[]) => range[0] - newInterval[1]);
+        if (intervals[firstPosition][0] === newInterval[0]) {
+            copy(intervals, 0, firstPosition, retval);
+            if (secondPosition !== end) {
+                if (intervals[secondPosition][0] === newInterval[1]) {
+                    retval.push([newInterval[0], intervals[secondPosition][1]]);
+                    copy(intervals, secondPosition + 1, end, retval);
+                } else {
+                    retval.push([newInterval[0], Math.max(newInterval[1], intervals[secondPosition - 1][1])]);
+                    copy(intervals, secondPosition, end, retval);
+                }
+            } else {
+                retval.push([newInterval[0], Math.max(newInterval[1], intervals[secondPosition - 1][1])]);
+            }
+        } else {
+            let range = [...newInterval];
+            let previousPosition = firstPosition - 1;
+            if (previousPosition >= 0) {
+                if (intervals[previousPosition][1] < newInterval[0]) {
+                    copy(intervals, 0, firstPosition, retval);
+                } else {
+                    range[0] = intervals[previousPosition][0];
+                    copy(intervals, 0, previousPosition, retval);
+                }
+            }
+            if (secondPosition !== end) {
+                if (intervals[secondPosition][0] === newInterval[1]) {
+                    range[1] = intervals[secondPosition][1];
+                    retval.push(range);
+                    copy(intervals, secondPosition + 1, end, retval);
+                } else {
+                    previousPosition = secondPosition - 1;
+                    if (previousPosition >= 0) {
+                        range[1] = Math.max(newInterval[1], intervals[previousPosition][1]);
+                    }
+                    retval.push(range);
+                    copy(intervals, secondPosition, end, retval);
+                }
+            } else {
+                range[1] = Math.max(newInterval[1], intervals[secondPosition - 1][1]);
+                retval.push(range);
+            }
+        }
+    } else {
+        --firstPosition;
+        if (firstPosition >= 0) {
+            if (intervals[firstPosition][1] < newInterval[0]) {
+                copy(intervals, 0, end, retval);
+                retval.push(newInterval);
+            } else {
+                copy(intervals, 0, firstPosition, retval);
+                retval.push([intervals[firstPosition][0], Math.max(intervals[firstPosition][1], newInterval[1])]);
+            }
+        } else {
+            retval.push([...newInterval]);
+        }
+    }
+    return retval;
+}
