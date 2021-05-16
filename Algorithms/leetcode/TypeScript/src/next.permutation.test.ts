d74d24e7ea3b4dfaf97777185c473b5607ce2f80
cyril-gao <yuanming.gao@gmail.com>
2021-05-16 19:03:52 -0400

    60. Permutation Sequence

--- Algorithms/leetcode/TypeScript/src/next.permutation.test.ts
+++ Algorithms/leetcode/TypeScript/src/next.permutation.test.ts
@@ -1,4 +1,4 @@
-import { nextPermutation } from './next.permutation';
+import { nextPermutation, getPermutation } from './next.permutation';
 
 test(
     "check the result value of the call to the function nextPermutation",
@@ -30,5 +30,40 @@ test(
         nums = [6, 3, 4, 7, 1, 2];
         nextPermutation(nums);
         expect(nums).toEqual([6, 3, 4, 7, 2, 1]);
+    }
+);
+
+test(
+    "check the result value of the call to the function getPermutation",
+    () => {
+        let expectation = [
+            '1234',
+            '1243',
+            '1324',
+            '1342',
+            '1423',
+            '1432',
+            '2134',
+            '2143',
+            '2314',
+            '2341',
+            '2413',
+            '2431',
+            '3124',
+            '3142',
+            '3214',
+            '3241',
+            '3412',
+            '3421',
+            '4123',
+            '4132',
+            '4213',
+            '4231',
+            '4312',
+            '4321'
+        ];
+        for (let k = 0; k < expectation.length; ++k) {
+            expect(getPermutation(4, k + 1)).toEqual(expectation[k]);
+        }
     }
 );
--- Algorithms/leetcode/TypeScript/src/next.permutation.ts
+++ Algorithms/leetcode/TypeScript/src/next.permutation.ts
@@ -34,3 +34,58 @@ export function nextPermutation(nums: number[]): void {
         }
     }
 }
+
+
+/*
+The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+1. "123"
+2. "132"
+3. "213"
+4. "231"
+5. "312"
+6. "321"
+
+Given n and k, return the kth permutation sequence.
+*/
+
+function getCandidate(index: number, used: boolean[]): number {
+    for (let i = 1, j = 0; i < used.length; ++i) {
+        if (!used[i]) {
+            if (++j === index) {
+                used[i] = true;
+                return i;
+            }
+        }
+    }
+    throw new Error('Bug');
+}
+
+export function getPermutation(n: number, k: number): string {
+    if (n > 0 && k > 0) {
+        if (n > 1) {
+            let result = [];
+            let cache = new Array<number>(n);
+            cache[0] = cache[1] = 1;
+            for (let i = 2; i < n; ++i) {
+                cache[i] = i * cache[i - 1];
+            }
+            let used = new Array<boolean>(n + 1);
+            used.fill(false);
+            for (let i = n - 1; i > 1; --i) {
+                let quotient = Math.floor((k + cache[i] - 1) / cache[i]);
+                k = k % cache[i];
+                if (k === 0) {
+                    k = cache[i];
+                }
+                result.push(getCandidate(quotient, used));
+            }
+            result.push(getCandidate(k, used));
+            result.push(getCandidate(1, used));
+            return result.join('');
+        }
+        return '1';
+    }
+    throw new Error(`One of the two input numbers ${n}, ${k} is less than 0`);
+}
