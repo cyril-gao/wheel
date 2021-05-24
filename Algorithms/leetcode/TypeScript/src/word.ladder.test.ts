d330bbfbf31c8450b6743d67435253e803445fd7
cyril-gao <yuanming.gao@gmail.com>
2021-05-24 18:22:20 -0400

    126. Word Ladder II; 127. Word Ladder

--- Algorithms/leetcode/TypeScript/src/word.ladder.test.ts
+++ Algorithms/leetcode/TypeScript/src/word.ladder.test.ts
@@ -1,4 +1,27 @@
-import { ladderLength } from "./word.ladder";
+import { findLadders, ladderLength } from "./word.ladder";
+
+test(
+    "check the result value of the call to the function findLadders",
+    () => {
+        let beginWord = "hit";
+        let endWord = "cog";
+        let wordList = ["hot","dot","dog","lot","log","cog"];
+        let result = findLadders(beginWord, endWord, wordList);
+        expect(result.length).toEqual(2);
+
+        beginWord = "red";
+        endWord = "tax";
+        wordList = ["ted", "tex", "red", "tax", "tad", "den", "rex", "pee", "rad", "rax"];
+        result = findLadders(beginWord, endWord, wordList);
+        expect(result.length).toEqual(6);
+
+        beginWord = "hit";
+        endWord = "pig";
+        wordList = ["hot","dot","dog","lot","log, pig"];
+        result = findLadders(beginWord, endWord, wordList);
+        expect(result.length).toEqual(0);
+    }
+);
 
 test(
     "check the result value of the call to the function ladderLength",
--- Algorithms/leetcode/TypeScript/src/word.ladder.ts
+++ Algorithms/leetcode/TypeScript/src/word.ladder.ts
@@ -1,62 +1,180 @@
-function getLetterSet(...words: string[]): Set<number> {
-    let set = new Set<number>();
-    for (let word of words) {
-        for (let c of word) {
-            set.add(c.charCodeAt(0));
+/*
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of
+words beginWord -> s1 -> s2 -> ... -> sk such that:
+    1. Every adjacent pair of words differs by a single letter.
+    2. Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+    3. sk == endWord
+
+Given two words, beginWord and endWord, and a dictionary wordList, return all the shortest transformation
+sequences from beginWord to endWord, or an empty list if no such sequence exists. Each sequence should be
+returned as a list of the words [beginWord, s1, s2, ..., sk].
+
+
+Example 1:
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+Output: [["hit","hot","dot","dog","cog"],["hit","hot","lot","log","cog"]]
+Explanation: There are 2 shortest transformation sequences:
+"hit" -> "hot" -> "dot" -> "dog" -> "cog"
+"hit" -> "hot" -> "lot" -> "log" -> "cog"
+
+Example 2:
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+Output: []
+Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+*/
+
+function getLetterSets(wordList: string[]): Set<number>[] {
+    let n = wordList[0].length;
+    let retval = new Array<Set<number>>(n);
+    for (let i = 0; i < n; ++i) {
+        let set = new Set<number>();
+        for (let j = 0; j < wordList.length; ++j) {
+            set.add(wordList[j].charCodeAt(i));
+        }
+        retval[i] = set;
+    }
+    return retval;
+}
+
+function getWordSet(wordList: string[]): Set<string> {
+    let retval = new Set<string>();
+    for (let w of wordList) {
+        retval.add(w);
+    }
+    return retval;
+}
+
+enum Color {
+    WHITE,
+    GRAY,
+    BLACK
+}
+
+class State {
+    public parent: Set<string>;
+    constructor(
+        public self: string,
+        parent: string | null = null,
+        public distance: number = 0,
+        public color: Color = Color.GRAY
+    ) {
+        this.parent = new Set<string>();
+        if (parent) {
+            this.parent.add(parent);
         }
     }
-    return set;
 }
 
-function getWordSet(...words: string[]): Set<string> {
-    let set = new Set<string>();
-    for (let word of words) {
-        set.add(word);
-    }
-    return set;
-}
-
-function getNeighbours(word: string, letters: Set<number>, wordSet: Set<String>): string[] {
-    let retval: string[] = [];
+function getNeighbours(word: string, candidates: Set<string>, letterSets: Set<number>[]): string[] {
+    let retval = [];
     for (let i = 0; i < word.length; ++i) {
+        let c = word.charCodeAt(i);
         let left = word.substring(0, i);
-        let right = word.substring(i+1);
-        let original = word[i].charCodeAt(0);
-        for (let c of letters) {
-            if (c != original) {
-                let newWord = left + String.fromCharCode(c) + right;
-                if (wordSet.has(newWord)) {
-                    retval.push(newWord);
+        let right = word.substring(i + 1);
+        for (let j of letterSets[i]) {
+            if (j != c) {
+                let w = left + String.fromCharCode(j) + right;
+                if (candidates.has(w)) {
+                    retval.push(w);
                 }
             }
         }
     }
     return retval;
 }
+
+function rebuildLadders(beginWord: string, endWord: string, states: Map<string, State>): string[][] {
+    let retval = [];
+    if (endWord !== beginWord) {
+        for (let p of states.get(endWord).parent) {
+            for (let l of rebuildLadders(beginWord, p, states)) {
+                l.push(endWord);
+                retval.push(l);
+            }
+        }
+    } else {
+        retval.push([beginWord]);
+    }
+    return retval;
+}
+
+export function findLadders(beginWord: string, endWord: string, wordList: string[]): string[][] {
+    let retval = [];
+    let candidates = getWordSet(wordList);
+    if (candidates.has(endWord)) {
+        let letterSets = getLetterSets(wordList);
+        let states = new Map<string, State>();
+        states.set(beginWord, new State(beginWord));
+        let queue = [states.get(beginWord)];
+        while (queue.length !== 0) {
+            let currentState = queue.shift();
+            let distance = currentState.distance + 1;
+            for (let neighbour of getNeighbours(currentState.self, candidates, letterSets)) {
+                if (!states.has(neighbour)) {
+                    let nextState = new State(neighbour, currentState.self, distance);
+                    states.set(neighbour, nextState);
+                    queue.push(nextState);
+                } else {
+                    let previousState = states.get(neighbour);
+                    if (previousState.distance === distance) {
+                        previousState.parent.add(currentState.self);
+                    }
+                }
+            }
+            currentState.color = Color.BLACK;
+        }
+        if (states.has(endWord)) {
+            retval = rebuildLadders(beginWord, endWord, states);
+        }
+    }
+    return retval;
+}
+
+
+/*
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+    1. Every adjacent pair of words differs by a single letter.
+    2. Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+    3. sk == endWord
+
+Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+
+Example 1:
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+Output: 5
+Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+
+Example 2:
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+Output: 0
+Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+*/
 
 export function ladderLength(beginWord: string, endWord: string, wordList: string[]): number {
     let retval = 0;
-    let letters = getLetterSet(beginWord, endWord, ...wordList);
-    let words = getWordSet(...wordList);
-    if (words.has(endWord)) {
-        let visited = new Set<string>();
-        let queue : [string, number][] = [[beginWord, 1]];
-        while (queue.length > 0) {
-            let front = queue.shift();
-            visited.add(front[0]);
-            let distance = front[1];
-            let neighbours = getNeighbours(front[0], letters, words);
-            for (let n of neighbours) {
-                if (n === endWord) {
-                    retval = distance + 1;
-                    queue = [];
-                    break;
-                }
-                if (!visited.has(n)) {
-                    queue.push([n, distance + 1]);
+    let candidates = getWordSet(wordList);
+    if (candidates.has(endWord)) {
+        let letterSets = getLetterSets(wordList);
+        let states = new Map<string, State>();
+        states.set(beginWord, new State(beginWord));
+        let queue = [states.get(beginWord)];
+        while (queue.length !== 0) {
+            let currentState = queue.shift();
+            let distance = currentState.distance + 1;
+            for (let neighbour of getNeighbours(currentState.self, candidates, letterSets)) {
+                if (!states.has(neighbour)) {
+                    let nextState = new State(neighbour, currentState.self, distance);
+                    states.set(neighbour, nextState);
+                    queue.push(nextState);
                 }
             }
+            currentState.color = Color.BLACK;
+        }
+        let lastState = states.get(endWord);
+        if (lastState !== undefined && lastState.color === Color.BLACK) {
+            retval = lastState.distance + 1;
         }
     }
     return retval;
-};
+}
