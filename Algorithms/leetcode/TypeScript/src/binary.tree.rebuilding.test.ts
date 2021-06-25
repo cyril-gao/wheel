21eb3b71695ccce4a4d7148dc6206a5346705bd8
cyril-gao <yuanming.gao@gmail.com>
2021-06-24 23:50:48 -0400

    109. Convert Sorted List to Binary Search Tree

--- Algorithms/leetcode/TypeScript/src/binary.tree.rebuilding.test.ts
+++ Algorithms/leetcode/TypeScript/src/binary.tree.rebuilding.test.ts
@@ -1,5 +1,5 @@
 import { TreeNode } from './tree.node';
-import { buildTree } from './binary.tree.rebuilding';
+import { buildTree, ListNode, sortedListToBST } from './binary.tree.rebuilding';
 
 test(
     "check the result value of the call to the function buildTree",
@@ -26,5 +26,57 @@ test(
         expect(result.val).toEqual(-1);
         expect(result.left).toEqual(null);
         expect(result.right).toEqual(null);
+    }
+);
+
+test(
+    "check the result value of the call to the function sortedListToBST",
+    () => {
+        {
+            let n1 = new ListNode(1);
+            let n2 = new ListNode(2);
+            let n3 = new ListNode(3);
+            let n4 = new ListNode(4);
+            let n5 = new ListNode(5);
+            n1.next = n2;
+            n2.next = n3;
+            n3.next = n4;
+            n4.next = n5;
+
+            let result = sortedListToBST(n1);
+            expect(result !== null).toBe(true);
+            expect(result.val).toEqual(3);
+            expect(result.left.val).toEqual(2);
+            expect(result.right.val).toEqual(5);
+            expect(result.left.left.val).toEqual(1);
+            expect(result.left.right).toBeNull();
+            expect(result.left.left.left).toBeNull();
+            expect(result.left.left.right).toBeNull();
+            expect(result.right.left.val).toEqual(4);
+            expect(result.right.right).toBeNull();
+            expect(result.right.left.left).toBeNull();
+            expect(result.right.left.right).toBeNull();
+        }
+        {
+            let n1 = new ListNode(1);
+            let n2 = new ListNode(2);
+            let n3 = new ListNode(3);
+            let n4 = new ListNode(4);
+            n1.next = n2;
+            n2.next = n3;
+            n3.next = n4;
+
+            let result = sortedListToBST(n1);
+            expect(result !== null).toBe(true);
+            expect(result.val).toEqual(3);
+            expect(result.left.val).toEqual(2);
+            expect(result.right.val).toEqual(4);
+            expect(result.left.left.val).toEqual(1);
+            expect(result.left.right).toBeNull();
+            expect(result.left.left.left).toBeNull();
+            expect(result.left.left.right).toBeNull();
+            expect(result.right.left).toBeNull();
+            expect(result.right.right).toBeNull();
+        }
     }
 );
--- Algorithms/leetcode/TypeScript/src/binary.tree.rebuilding.ts
+++ Algorithms/leetcode/TypeScript/src/binary.tree.rebuilding.ts
@@ -41,3 +41,60 @@ export function buildTree(inorder: number[], postorder: number[]): TreeNode | nu
     }
     return retval;
 }
+
+export class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+}
+
+/*
+Given the head of a singly linked list where elements are sorted in ascending order,
+convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which
+the depth of the two subtrees of every node never differ by more than 1.
+*/
+
+function getNode(head: ListNode, index: number): ListNode {
+    let retval = head;
+    while (index > 0) {
+        retval = retval.next;
+        --index;
+    }
+    return retval;
+}
+
+function length(head: ListNode | null): number {
+    let retval = 0;
+    for (let i = head; i !== null; i = i.next) {
+        ++retval;
+    }
+    return retval;
+}
+
+function buildBST(head: ListNode | null, length: number): TreeNode | null {
+    let retval: TreeNode = null;
+    if (length > 1) {
+        let n = Math.floor(length / 2);
+        let node = getNode(head!, n);
+        retval = new TreeNode(node.val);
+        retval.left = buildBST(head, n);
+        retval.right = buildBST(node.next, length - n - 1);
+    } else if (length > 0) {
+        retval = new TreeNode(head.val);
+    }
+    return retval;
+}
+
+export function sortedListToBST(head: ListNode | null): TreeNode | null {
+    let retval = null;
+    if (head !== null) {
+        let n = length(head);
+        retval = buildBST(head, n);
+    }
+    return retval;
+}
