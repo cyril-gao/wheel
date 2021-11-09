f249d3a526ef25323ec43ef3079b8e4be23099ca
Cyril Gao <cyril.gao@outlook.com>
2021-11-09 11:38:25 -0500

    rank those quick sort algorithms in the order of performance

--- Algorithms/sort/sort_test.cpp
+++ Algorithms/sort/sort_test.cpp
@@ -6,6 +6,7 @@
 #include <random>
 #include <chrono>
 #include <stdexcept>
+#include <string>
 
 #include "insertion_sort.h"
 #include "merge_sort.h"
@@ -14,8 +15,40 @@
 #include "counting_sort.h"
 #include "str_sort.h"
 
+struct PerformanceRecord
+{
+    std::string function_name;
+    double duration;
 
-#define EXECUTE(data, function, showing_duration)                             \
+    PerformanceRecord(const char * name, double d) : function_name(name), duration(d) {}
+    PerformanceRecord() : PerformanceRecord("", 0.0) {}
+
+    bool operator<(PerformanceRecord const& other) const noexcept
+    {
+        return duration < other.duration;
+    }
+};
+
+void print_record(const char * name, double duration)
+{
+    printf("%37s: %f seconds\n", name, duration);
+}
+
+struct PerformanceRecorder
+{
+    std::vector<PerformanceRecord> records;
+    void save(const char * name, double duration)
+    {
+        records.emplace_back(name, duration);
+    }
+};
+
+struct NullPerformanceRecorder
+{
+    void save(const char *, double) {}
+};
+
+#define EXECUTE(data, function, showing_duration, performance_recorder)       \
     do                                                                        \
     {                                                                         \
         auto copy(data);                                                      \
@@ -29,8 +62,9 @@
         double duration = std::chrono::duration<double>(end - begin).count(); \
         if (showing_duration)                                                 \
         {                                                                     \
-            printf("%37s: %f seconds\n", #function, duration);                \
+            print_record(#function, duration);                                \
         }                                                                     \
+        performance_recorder.save(#function, duration);                       \
     } while (false)
 
 inline void print_separator()
@@ -50,25 +84,27 @@ void verification_test()
         v = dist(rd);
     }
 
-    EXECUTE(data, insertion_sort, false);
-    EXECUTE(data, merge_sort, false);
-    EXECUTE(data, heap_sort, false);
-    EXECUTE(data, two_way_quick_sort<V1_1Partitioner>, false);
-    EXECUTE(data, two_way_quick_sort<V1_2Partitioner>, false);
-    EXECUTE(data, two_way_quick_sort<V1_3Partitioner>, false);
-    EXECUTE(data, two_way_quick_sort<V1_4Partitioner>, false);
-    EXECUTE(data, v2::two_way_quick_sort, false);
-    EXECUTE(data, two_way_quick_sort<V2_1Partitioner>, false);
-    EXECUTE(data, two_way_quick_sort<V2_2Partitioner>, false);
-    EXECUTE(data, three_way_quick_sort<V3_1Partitioner>, false);
-    EXECUTE(data, three_way_quick_sort<V3_2Partitioner>, false);
-    EXECUTE(data, three_way_quick_sort<V3_3Partitioner>, false);
-    EXECUTE(data, three_way_quick_sort<V3_4Partitioner>, false);
+    [[maybe_unused]] NullPerformanceRecorder recorder;
+    EXECUTE(data, insertion_sort, false, recorder);
+    EXECUTE(data, merge_sort, false, recorder);
+    EXECUTE(data, heap_sort, false, recorder);
+    EXECUTE(data, two_way_quick_sort<V1_1Partitioner>, false, recorder);
+    EXECUTE(data, two_way_quick_sort<V1_2Partitioner>, false, recorder);
+    EXECUTE(data, two_way_quick_sort<V1_3Partitioner>, false, recorder);
+    EXECUTE(data, two_way_quick_sort<V1_4Partitioner>, false, recorder);
+    EXECUTE(data, v2::two_way_quick_sort, false, recorder);
+    EXECUTE(data, two_way_quick_sort<V2_1Partitioner>, false, recorder);
+    EXECUTE(data, two_way_quick_sort<V2_2Partitioner>, false, recorder);
+    EXECUTE(data, three_way_quick_sort<V3_1Partitioner>, false, recorder);
+    EXECUTE(data, three_way_quick_sort<V3_2Partitioner>, false, recorder);
+    EXECUTE(data, three_way_quick_sort<V3_3Partitioner>, false, recorder);
+    EXECUTE(data, three_way_quick_sort<V3_4Partitioner>, false, recorder);
 }
 
 template <typename T>
 void cache_test()
 {
+    [[maybe_unused]] NullPerformanceRecorder recorder;
     for (size_t i = 16; i <= 1024; i += i)
     {
         std::random_device rd;
@@ -80,9 +116,9 @@ void cache_test()
         }
         print_separator();
         printf("%zu %zu-byte integers:\n", i, sizeof(T));
-        EXECUTE(data, insertion_sort, true);
-        EXECUTE(data, heap_sort, true);
-        EXECUTE(data, std::sort, true);
+        EXECUTE(data, insertion_sort, true, recorder);
+        EXECUTE(data, heap_sort, true, recorder);
+        EXECUTE(data, std::sort, true, recorder);
         printf("\n");
     }
 }
@@ -109,6 +145,7 @@ std::vector<std::string> create_string_buffer(
 
 void str_sort_test(const size_t buf_size)
 {
+    [[maybe_unused]] NullPerformanceRecorder recorder;
     std::uniform_int_distribution<int16_t> value_dist(' ', '~');
     std::uniform_int_distribution<size_t> length_dist(4, 128);
     using details::V4_1Partitioner;
@@ -118,16 +155,17 @@ void str_sort_test(const size_t buf_size)
         print_separator();
         printf("%zu strings:\n", size);
         auto data = create_string_buffer(size, value_dist, length_dist);
-        EXECUTE(data, msd_sort, true);
-        EXECUTE(data, str_sort<V4_1Partitioner>, true);
-        EXECUTE(data, str_sort<V4_2Partitioner>, true);
-        EXECUTE(data, std::sort, true);
+        EXECUTE(data, msd_sort, true, recorder);
+        EXECUTE(data, str_sort<V4_1Partitioner>, true, recorder);
+        EXECUTE(data, str_sort<V4_2Partitioner>, true, recorder);
+        EXECUTE(data, std::sort, true, recorder);
     }
 }
 
 template <typename T>
 void heap_sort_vs_merge_sort()
 {
+    [[maybe_unused]] NullPerformanceRecorder recorder;
     for (size_t i = 64; i <= 4096; i += i)
     {
         std::random_device rd;
@@ -139,8 +177,8 @@ void heap_sort_vs_merge_sort()
         }
         print_separator();
         printf("%zu %zu-byte integers:\n", i, sizeof(T));
-        EXECUTE(data, heap_sort, true);
-        EXECUTE(data, merge_sort, true);
+        EXECUTE(data, heap_sort, true, recorder);
+        EXECUTE(data, merge_sort, true, recorder);
         printf("\n");
     }
 }
@@ -156,30 +194,37 @@ void quick_sort_extreme_cases(const size_t buf_size)
         {
             v = static_cast<T>(rand()) % i;
         }
-        print_separator();
-        printf("1/%u:\n", i);
-        EXECUTE(data, std::sort, true);
-        EXECUTE(data, v2::two_way_quick_sort, true);
-        //EXECUTE(data, v3::three_way_quick_sort, true);
-        EXECUTE(data, three_way_quick_sort<V3_1Partitioner>, true);
-        EXECUTE(data, three_way_quick_sort<V3_2Partitioner>, true);
-        EXECUTE(data, three_way_quick_sort<V3_3Partitioner>, true);
-        EXECUTE(data, three_way_quick_sort<V3_4Partitioner>, true);
+
+        PerformanceRecorder recorder;
+        EXECUTE(data, std::sort, false, recorder);
+        EXECUTE(data, v2::two_way_quick_sort, false, recorder);
+        //EXECUTE(data, v3::three_way_quick_sort, false, recorder);
+        EXECUTE(data, three_way_quick_sort<V3_1Partitioner>, false, recorder);
+        EXECUTE(data, three_way_quick_sort<V3_2Partitioner>, false, recorder);
+        EXECUTE(data, three_way_quick_sort<V3_3Partitioner>, false, recorder);
+        EXECUTE(data, three_way_quick_sort<V3_4Partitioner>, false, recorder);
         /*if (i > 4)*/ {
             // may crash !!!
-            EXECUTE(data, two_way_quick_sort<V1_1Partitioner>, true);
-            EXECUTE(data, two_way_quick_sort<V1_2Partitioner>, true);
-            EXECUTE(data, two_way_quick_sort<V1_3Partitioner>, true);
-            EXECUTE(data, two_way_quick_sort<V1_4Partitioner>, true);
+            EXECUTE(data, two_way_quick_sort<V1_1Partitioner>, false, recorder);
+            EXECUTE(data, two_way_quick_sort<V1_2Partitioner>, false, recorder);
+            EXECUTE(data, two_way_quick_sort<V1_3Partitioner>, false, recorder);
+            EXECUTE(data, two_way_quick_sort<V1_4Partitioner>, false, recorder);
         }
-        EXECUTE(data, two_way_quick_sort<V2_1Partitioner>, true);
+        EXECUTE(data, two_way_quick_sort<V2_1Partitioner>, false, recorder);
         /*if (i > 4)*/ {
             // may crash !!!
-            EXECUTE(data, two_way_quick_sort<V2_2Partitioner>, true);
+            EXECUTE(data, two_way_quick_sort<V2_2Partitioner>, false, recorder);
         }
         v3::ThreeWayQuickSort<16, 8> three_way_quick_sort;
-        EXECUTE(data, three_way_quick_sort, true);
-        EXECUTE(data, v4::hybird_quick_sort<16>, true);
+        EXECUTE(data, three_way_quick_sort, false, recorder);
+        EXECUTE(data, v4::hybird_quick_sort<16>, false, recorder);
+    
+        print_separator();
+        printf("1/%u:\n", i);
+        std::sort(recorder.records.begin(), recorder.records.end());
+        for (auto const& record : recorder.records) {
+            print_record(record.function_name.c_str(), record.duration);
+        }
         printf("\n");
     }
 }
@@ -195,34 +240,40 @@ void performance_test(const size_t buf_size, const double factor)
         v = dist(rd);
     }
 
+    PerformanceRecorder recorder;
+    EXECUTE(data, std::sort, false, recorder);
+    EXECUTE(data, v2::two_way_quick_sort, false, recorder);
+    //EXECUTE(data, v3::three_way_quick_sort, true);
+    EXECUTE(data, two_way_quick_sort<V1_1Partitioner>, false, recorder);
+    EXECUTE(data, two_way_quick_sort<V1_2Partitioner>, false, recorder);
+    EXECUTE(data, two_way_quick_sort<V1_3Partitioner>, false, recorder);
+    EXECUTE(data, two_way_quick_sort<V1_4Partitioner>, false, recorder);
+    EXECUTE(data, two_way_quick_sort<V2_1Partitioner>, false, recorder);
+    EXECUTE(data, two_way_quick_sort<V2_2Partitioner>, false, recorder);
+    EXECUTE(data, three_way_quick_sort<V3_1Partitioner>, false, recorder);
+    EXECUTE(data, three_way_quick_sort<V3_2Partitioner>, false, recorder);
+    EXECUTE(data, three_way_quick_sort<V3_3Partitioner>, false, recorder);
+    EXECUTE(data, three_way_quick_sort<V3_4Partitioner>, false, recorder);
+    EXECUTE(data, merge_sort, false, recorder);
+    EXECUTE(data, heap_sort, false, recorder);
+    v3::ThreeWayQuickSort<16, 8> three_way_quick_sort_1;
+    EXECUTE(data, three_way_quick_sort_1, false, recorder);
+    v3::ThreeWayQuickSort<16, 16> three_way_quick_sort_2;
+    EXECUTE(data, three_way_quick_sort_2, false, recorder);
+    v3::ThreeWayQuickSort<8, 8> three_way_quick_sort_3;
+    EXECUTE(data, three_way_quick_sort_3, false, recorder);
+    v3::ThreeWayQuickSort<4, 8> three_way_quick_sort_4;
+    EXECUTE(data, three_way_quick_sort_4, false, recorder);
+    EXECUTE(data, v4::hybird_quick_sort<64>, false, recorder);
+    EXECUTE(data, v4::hybird_quick_sort<32>, false, recorder);
+    EXECUTE(data, v4::hybird_quick_sort<16>, false, recorder);
+    EXECUTE(data, v4::hybird_quick_sort<8>, false, recorder);
     print_separator();
     printf("factor: %f\n", factor);
-    EXECUTE(data, std::sort, true);
-    EXECUTE(data, v2::two_way_quick_sort, true);
-    //EXECUTE(data, v3::three_way_quick_sort, true);
-    EXECUTE(data, two_way_quick_sort<V1_1Partitioner>, true);
-    EXECUTE(data, two_way_quick_sort<V1_2Partitioner>, true);
-    EXECUTE(data, two_way_quick_sort<V1_3Partitioner>, true);
-    EXECUTE(data, two_way_quick_sort<V1_4Partitioner>, true);
-    EXECUTE(data, two_way_quick_sort<V2_1Partitioner>, true);
-    EXECUTE(data, two_way_quick_sort<V2_2Partitioner>, true);
-    EXECUTE(data, three_way_quick_sort<V3_1Partitioner>, true);
-    EXECUTE(data, three_way_quick_sort<V3_2Partitioner>, true);
-    EXECUTE(data, three_way_quick_sort<V3_3Partitioner>, true);
-    EXECUTE(data, three_way_quick_sort<V3_4Partitioner>, true);
-    EXECUTE(data, merge_sort, true);
-    EXECUTE(data, heap_sort, true);
-    v3::ThreeWayQuickSort<16, 8> three_way_quick_sort_1;
-    EXECUTE(data, three_way_quick_sort_1, true);
-    v3::ThreeWayQuickSort<16, 16> three_way_quick_sort_2;
-    EXECUTE(data, three_way_quick_sort_2, true);
-    v3::ThreeWayQuickSort<8, 8> three_way_quick_sort_3;
-    EXECUTE(data, three_way_quick_sort_3, true);
-    v3::ThreeWayQuickSort<4, 8> three_way_quick_sort_4;
-    EXECUTE(data, three_way_quick_sort_4, true);
-    EXECUTE(data, v4::hybird_quick_sort<32>, true);
-    EXECUTE(data, v4::hybird_quick_sort<16>, true);
-    EXECUTE(data, v4::hybird_quick_sort<8>, true);
+    std::sort(recorder.records.begin(), recorder.records.end());
+    for (auto const& record : recorder.records) {
+        print_record(record.function_name.c_str(), record.duration);
+    }
 }
 
 template <typename T>
@@ -237,10 +288,11 @@ void counting_sort_test_cast_for_bytes(size_t buf_size)
     for (auto& v : data) {
         v = static_cast<T>(dist(rd));
     }
+    [[maybe_unused]] NullPerformanceRecorder recorder;
     print_separator();
     printf("%zu %s:\n", buf_size, typeid(T).name());
-    EXECUTE(data, std::sort, true);
-    EXECUTE(data, integer_sort, true);
+    EXECUTE(data, std::sort, true, recorder);
+    EXECUTE(data, integer_sort, true, recorder);
 }
 
 template <typename T>
@@ -255,10 +307,11 @@ void counting_sort_test_cast(size_t buf_size)
     for (auto& v : data) {
         v = dist(rd);
     }
+    [[maybe_unused]] NullPerformanceRecorder recorder;
     print_separator();
     printf("%zu %s:\n", buf_size, typeid(T).name());
-    EXECUTE(data, std::sort, true);
-    EXECUTE(data, integer_sort, true);
+    EXECUTE(data, std::sort, true, recorder);
+    EXECUTE(data, integer_sort, true, recorder);
 }
 
 void counting_sort_test(size_t buf_size)
@@ -293,6 +346,7 @@ int main(int argc, char* argv[])
 
     try {
         {
+            [[maybe_unused]] NullPerformanceRecorder recorder;
             std::vector<std::string> data = {
                 "98764", "3745672", "1236347", "3", "38374356", "9476453", "39072534", "7456623",
                 "34866352", "5678452", "38456342", "5623434", "50798567", "263454", "8456456",
@@ -301,11 +355,11 @@ int main(int argc, char* argv[])
                 "1233545", "426346", "345634", "456345", "23423", "435234", "564745",
                 "2", "9", "8", "7", "6", "5", "4", "1"
             };
-            EXECUTE(data, msd_sort, false);
+            EXECUTE(data, msd_sort, false, recorder);
             using details::V4_1Partitioner;
             using details::V4_2Partitioner;
-            EXECUTE(data, str_sort<V4_1Partitioner>, false);
-            EXECUTE(data, str_sort<V4_2Partitioner>, false);
+            EXECUTE(data, str_sort<V4_1Partitioner>, false, recorder);
+            EXECUTE(data, str_sort<V4_2Partitioner>, false, recorder);
         }
         verification_test<int16_t>();
         verification_test<int64_t>();
